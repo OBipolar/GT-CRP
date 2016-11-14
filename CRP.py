11f8ffb2602ab890a4935b8956c0fa72cc4215bb
OBipolar <shaohuixu13@gatech.edu>
2016-11-14 00:15:49 -0500

    Add boilerplate for the project

---
+++ CRP.py
@@ -0,0 +1,14 @@
+class CRP:
+
+    def __init__(self):
+
+    def setupServer(self):
+
+    def sendPacket(self, data):
+
+    def readData(self):
+
+    def connectTo(self, selfPort, serverIP, serverPort):
+
+    def close(self):
+
---
+++ crpPacket.py
@@ -0,0 +1,15 @@
+# CRP Packet Serializer and Deserializer
+# Together with Util functions
+
+def str2Bits(s):
+
+def bits2Str(bits):
+
+# Serialize packet in dict form to a string
+def packetSerialize(packet):
+
+# Deserialize a packet in string form into dict
+def packetDeserialize(packetString):
+
+def f16CheckSum(packetString):
+
