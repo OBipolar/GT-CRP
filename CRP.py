7950a0bb5c94c491566c967be2ab95a3635272f7
DU-Xinnan <xduac@connect.ust.hk>
2016-11-21 11:00:50 -0500

    update listener

--- CRP.py
+++ CRP.py
@@ -3,22 +3,27 @@ from crpPacket import *
 import threading
 import sys
 import time
-
+import netaddr
 class CRP:
 
     def __init__(self):
     	self.sendingQueue = Queue()
     	self.notAckedQueue = Queue() # [packetString, timeStamp]
     	self.receiveBUffer = Queue()
+        self.expectedSeqNum = 0
     	self.packetSize = 1024
-    	self.buffsize = 65535
+        self.receiver_windowSize = 20
+        self.send_windowSize = 20
     	self.portNum = None
     	self.IP = socket.gethostbyname(socket.gethostname())
     	self.destination = None #[addr, port]
     	self.sender_seqNum = 0
     	self.receiver_seqNum = 0
     	self.close = False
-    	self.ackedNum = set()
+    	self.ackedNum = dict()
+        self.receivedSeqNum = []
+        self.lock = threading.Lock()
+        self.normalClose = None
 
     def setupServer(self,port):
         #three way handshake of receiver
@@ -30,7 +35,7 @@ class CRP:
     	    data, addr = self.dataSocket.recvfrom(24)
             synPacketDictFromClient = packetDeserialize(data)
             # print "get initail syn!"
-            if (synPacketDictFromClient['checksum'] == fletcherCheckSum(data,16) and synPacketDictFromClient['syn'] == 1):
+            if (synPacketDictFromClient['checksum'] == fletcherCheckSum(data["data"],16) and synPacketDictFromClient['syn'] == 1):
                 # create new
                 self.portNum = port
                 self.destination = addr
@@ -68,15 +73,122 @@ class CRP:
                 if packet["seqNum"] != 0:
                     self.notAckedQueue.push((packetString, time.time()))
 
+
     def receiver(self):
     	while 1:
             try:
                 time.sleep(0.1)
             except socket.timeout:
-                print "connection timeout"
+                print "connection timeout"#a connection cannot exeet the timeout limit
                 self.receiver_close()
             dataString, addr = self.dataSocket.recvfrom(self.packetSize)[0]
+            data = packetDeserialize(dataString)
+            print "Receive with SequenceNum: ", data["seqNum"]," ackNum: ",data["ackNum"], " ack_bit: ",data["ack"], " fin: ", data[fin]
+            #discard it if it's a duplicate packet
+            if data["seqNum"] in self.receivedSeqNum:
+                continue
+            #check sum
+            if data["checksum"] =  fletcherCheckSum(data["data"],16):
+                #the other side send ackNum = desired SequenceNum
+                #when ack is 1, whcih means CRP previously sent something
+                if data["ack"] == 1 && data["rst"]!=1:
+                    if str(data['ackNum']) in self.ackedNum:
+                        self.ackedNum[str(data['ackNum'])] += 1
+                    else:
+                        self.ackedNum[str(data['ackNum'])] = 1
+                    #remove the acked packet from the notAckedQueue    
+                    for index, packet in enumerate(self.notAckedQueue.list):
+                        if packet['seqNum'] == data["ackedNum"] - 1:
+                            self.notAckedQueue.remove(index)
+                            break
+                    self._check_nackQueue_retransmit()
+                    if len(data["data"]) != 0:
+                        self._push_to_Buffer(data)
+                        self._check_buffer_send_Ack(data)
+                #if receive NACK, retransmit the packet
+                elif data["ack"] == 1 && data["rst"]==1:
+                    for index, notAckPacket in enumerate(self.notAckedQueue.list):
+                        if notAckPacket["seqNum"] == data["ackNum"]:
+                            self.sendingQueue.push_front(self.notAckedQueue.remove(index))
+                            break
+                elif data["fin"] == 1:
+                    self._piggy_backing_send_ack(data)
+                    self.receiver_close()
+                else:
+                    if self._push_to_Buffer(data):
+                        self._check_buffer_send_Ack(data)
+            else:
+                #send NACK if checksum fails, send the received packet seqNum as ackNum
+                self.receiver_seqNum += 1
+                self._send_NACK(data["seqNum"]) #ack and rst means NACK
     
+    def _send_NACK(self,seqNum):
+        self._sendPacket("", {"ack": 1, "rst":1, "ackNum": seqNum})
+
+    def _check_buffer_send_Ack(self,data):
+        seqNuminBuff = [x['seqNum'] for x in self.receiveBUffer]
+
+        if data['seqNum'] = self.expectedSeqNum:
+            self.lock.require()
+            dataIndex = self.receiveBUffer.index(data['seqNum'])
+            if len(self.receiveBUffer.list)==1:
+                self.expectedSeqNum+=1
+            elif self.receiveBUffer.list[dataIndex + 1]['seqNum']  -self.expectedSeqNum >1:
+                self.expectedSeqNum+=1
+            else:
+                findBreak = False
+                for i in range(0,len(seqNuminBuff)-1):
+                    if seqNuminBuff[i] + 1 != seqNuminBuff[i+1]:
+                        self.expectedSeqNum = seqNuminBuff[i] + 1
+                        findBreak = True
+                        break
+                if not findBreak:
+                    self.expectedSeqNum = max(seqNuminBuff)+1
+            self._send_ack(self.expectedSeqNum)
+            self.lock.release()
+        else:
+            self._send_ack(self.expectedSeqNum)
+
+        
+
+    def _piggy_backing_send_ack(self,Mypacket):
+        '''
+        piggy backing send ack, attach ack to a packet we are going to send
+        '''
+        findOne = False
+        for index, packet in enumerate(self.sendingQueue.list):
+            if packet["ack"] == 0:
+                packet["ack"] = 1
+                packet["ackNum"] = Mypacket["seqNum"] + 1
+                findOne = True
+                break
+        if not findOne:
+            self._send_ack(Mypacket["seqNum"]+1)
+
+    def _send_ack(self, seqNum):
+        self.sender_seqNum += 1
+        rtpPacketDict["sourcePort"] = self.port
+        rtpPacketDict["destPort"] = self.destAddr[1]
+        self._sendPacket("",{"ackNum":seqNum, "seqNum":self.sender_seqNum})
+
+
+    def _push_to_Buffer(self,data):
+        if self.receiveBUffer.length() >= self.receiver_windowSize:
+            return False
+        if data["seqNum"] < expectedSeqNum or data["seqNum"] >= self.expectedSeqNum+self.receiver_windowSize:
+            return False
+        self.receiveBUffer.insert_inorder(data)#may should be inverse
+        return True
+
+    def _check_nackQueue_retransmit(self):
+        for key, value in self.ackedNum.iteritems():
+            if value >= 3:
+                for index, notAckPacket in enumerate(self.notAckedQueue.list):
+                    if notAckPacket["seqNum"] == key:
+                        self.sendingQueue.push_front(self.notAckedQueue.remove(index))
+                        self.ackedNum[key] = 0
+            
+                        
     def receiver_close(self):
         pass
 
@@ -87,7 +199,7 @@ class CRP:
     	packet["destPort"] = self.destination[1]
     	packet["data"] = data
     	packet["checksum"] = fletcherCheckSum(data,16)
-
+        
     	for key in header:
     		packet[key] = header[key]
     	sendString = normalPacketSerialize(packet)
@@ -103,7 +215,31 @@ class CRP:
     	pass
 
     def connectTo(self, selfPort, serverIP, serverPort):
-    	pass
+    	if netaddr.valid_ipv4(serverIP):
+            self.dataSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        elif netaddr.valid_ipv6(serverIP):
+            self.dataSocket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)            
+        else:
+            print ("IP Address not valid")
+            break
+        listen_addr = ("", selfPort)
+        self.dataSocket.bind(listen_addr)
+        # Init data socket
+        self.portNum = selfPort
+        self.destination = (serverIP, serverPort)
+        self.timeout = 2 #init timeout for packet resend is 2 seconds
+        self._sendPacket(packetDeserialize(str(bin(initialPacketSizeInByte))[2:], 4), {"syn":1})
+        ackPacketString = self._receive_packet()
+        ackPacket = packetDeserialize(ackPacketString)
+        if (ackPacket['checksum'] == fletcherCheckSum(ackPacketString,16) and ackPacket['syn'] == 1 and ackPacket['ack'] == 1):
+            self._sendPacket("", {"ack": 1})
+            # ------------FINISH THREE WAY HANDSHAKE--------------
+            tSender = threading.Thread(target=self.sender)
+            tSender.daemon = True
+            tSender.start()
+            tListener = threading.Thread(target=self.receiver)
+            tListener.daemon = True
+            tListener.start()
 
     def close(self):
     	pass
--- crpPacket.py
+++ crpPacket.py
@@ -104,138 +104,10 @@ def fletcherCheckSum(packetString, k):
         sum2 = (sum2+sum1)%255
     sum1 = bin(sum1)[2:]
     sum2 = bin(sum2<<8)[2:]
-    return sum2+sum1
-
-class Queue(list):
-    def __init__(self):
-        self.list = []
-
-    def push(self,item):
-        "Enqueue the 'item' into the queue"
-        self.list.insert(0,item)
-
-    def pop(self):
-        """
-          Dequeue the earliest enqueued item still in the queue. This
-          operation removes the item from the queue.
-        """
-        return self.list.pop()
-
-    def isEmpty(self):
-        "Returns true if the queue is empty"
-        return len(self.list) == 0
-
-    
-# Author: Shaohui Xu
-
-# CRP Packet Serializer and Deserializer
-# Together with Util functions
-
-BYTE_SIZE = 8
-FLETCHER_CONFIG = (16, 32, 64)
-PACKET_HEADER = ("sourcePort", "destPort", "seqNum", "ackNum", "headerLen", "ack", "rst", "syn", "fin", "recvWindowSize", "checksum")
-
-# TODO: documentation
-
-def str2Bits(s):
-    bits = ''
-    bitMask = '00000000'
-    for c in s:
-        charBits = bin(ord(c))[2:]
-        charBits = bitMask[len(bits):] + charBits
-        bits = bits + charBits
-    return bits
-
-def bits2Str(bits, byteLength):
-    s = ''
-    charList = []
-
-    if len(bits) <= byteLength * BYTE_SIZE:
-        bits = str('0' * (byteLength * BYTE_SIZE - len(bits))) + bits 
-    elif len(bits) > byteLength * BYTE_SIZE and byteLength != 0:
-        bits = bits[-byteLength * BYTE_SIZE]
-
-    for idx in range(len(bits) / BYTE_SIZE):
-        byte = bits[idx * BYTE_SIZE: (idx+1) * BYTE_SIZE]
-        charList.append(chr(int(''.join([str(bit) for bit in byte]), 2)))
-    s = s.join(charList)
-
-    return s
-
-# TODO: modify header strucutre for the line of headerlength, ack, syn, rst, fin to meet length requirement
-# Serialize normalized packet in fict from to a string
-def normalPacketSerialize(packet):
-    packetString = ""
-    packetString += bits2Str(str(bin(packet["sourcePort"]))[2:],2)
-    packetString += bits2Str(str(bin(packet["destPort"]))[2:],2)
-    packetString += bits2Str(str(bin(packet["seqNum"]))[2:],4)
-    packetString += bits2Str(str(bin(packet["ackNum"]))[2:],4)
-    packetString += bits2Str(str(bin(packet["headerLen"]))[2:] + str(bin(packet["ack"]))[2:] + str(bin(packet["rst"]))[2:] + str(bin(packet["syn"]))[2:] + str(bin(packet["fin"]))[2:], 1)
-    packetString += bits2Str(str(bin(packet["recvWindowSize"]))[2:], 3)
-    packetString += bits2Str(str(bin(packet["checksum"]))[2:],4)
-    if packet["headerLen"] != 0:
-        packetString += bits2Str(str(bin(packet["option"]))[2:], packet["headerLen"] * 4)
-    if "data" in packet:
-        packetString += packet["data"]    
-    return packetString
-
-# Serialize packet in dict form to a string
-def packetSerialize(packet):
-    packetString = ""
-    try:
-        # normalize header with empty fields
-        for field in PACKET_HEADER:
-            if field not in packet:
-                packet[field] = 0
-        packetString = normalPacketSerialize(packet)
-    except:
-        print "Unexpected error in crp packet serialization"
-        raise 
-    return packetString
-
-# Deserialize a packet in string form into dict
-def packetDeserialize(packetString):
-    packet = {}
-    try:
-        packetBitString = str2Bits(packetString)
-        packet["sourcePort"] = int(packetBitString[0:16], 2)
-        packet["destPort"] = int(packetBitString[16:32], 2)
-        packet["seqNum"] = int(packetBitString[32:64], 2)
-        packet["ackNum"] = int(packetBitString[64:96], 2)
-        packet["headerLen"] = int(packetBitString[96:100], 2)
-        packet["ack"] = int(packetBitString[100:101], 2)
-        packet["rst"] = int(packetBitString[101:102], 2)
-        packet["syn"] = int(packetBitString[102:103], 2)
-        packet["fin"] = int(packetBitString[103:104], 2)
-        packet["recvWindowSize"] = int(packetBitString[104:128], 2)
-        packet["checksum"] = int(packetBitString[128:160], 2)
-        if packet["headerLen"] != 0:
-            packet["option"] = int(packetBitString[160: (160 + packet["headerLen"]*4*BYTE_SIZE)], 2)
-        else:
-            packet["option"] = 0;
-        packet["data"] = packetString[20 + packet["headerLen"] * 4:]
-    except:
-        print "Unexpected error in crp packet string deserialization"
-        raise
-    return packet
-
-# Fletcher checksum
-# More details can be found on http://www.drdobbs.com/database/fletchers-checksum/184408761
-def fletcherCheckSum(packetString, k):
-    if k not in FLETCHER_CONFIG:
-        raise ValueError("Valid choices of k should be 16, 32 and 64")
-    sum1 = 0
-    sum2 = 0
-    count = len(packetString)/k
-    for index in range(count):
-        sum1 = (sum1 + packetString[count*k:(count+1)*k])%255
-        sum2 = (sum2+sum1)%255
-    sum1 = bin(sum1)[2:]
-    sum2 = bin(sum2<<8)[2:]
     zero16 = "0000000000000000"
     return sum2+sum1+zero16
-
-class Queue(list):
+#self defined queue class
+class Queue():
     def __init__(self):
         self.list = []
 
@@ -249,15 +121,27 @@ class Queue(list):
           operation removes the item from the queue.
         """
         return self.list.pop()
-
+    
+    def push_front(self,item):
+        self.list.append(item)
+        
     def isEmpty(self):
         "Returns true if the queue is empty"
         return len(self.list) == 0
     
     def insert_inorder(self,item):
         self.list.append(item)
-        self.list.sort()
-                    
+        self.list.sort(key=lambda x: x['seqNum']) 
+
+    def remove(self, pos):
+        return self.list.pop(pos)
+
+    def length(self):
+        return len(self.list)
+    
+        
+        
+
                     
                     
                     
