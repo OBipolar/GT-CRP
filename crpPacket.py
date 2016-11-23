33201db73f1d8af13fb9c80476feb0281db553e8
OBipolar <shaohuixu13@gatech.edu>
2016-11-22 22:27:09 -0500

    Testing util functions and add doc

--- crpPacket.py
+++ crpPacket.py
@@ -1,24 +1,28 @@
-# Author: Shaohui Xu
-
-# CRP Packet Serializer and Deserializer
-# Together with Util functions
+# Author: Shaohui Xu, Xinnan Du
 
 BYTE_SIZE = 8
 FLETCHER_CONFIG = (16, 32, 64)
 PACKET_HEADER = ("sourcePort", "destPort", "seqNum", "ackNum", "headerLen", "ack", "rst", "syn", "fin", "recvWindowSize", "checksum")
 
-# TODO: documentation
-
 def str2Bits(s):
+    """
+        Convert string to bits (represented using binary string)
+    """
     bits = ''
     bitMask = '00000000'
     for c in s:
         charBits = bin(ord(c))[2:]
-        charBits = bitMask[len(bits):] + charBits
+        charBits = bitMask[len(charBits):] + charBits
         bits = bits + charBits
     return bits
 
 def bits2Str(bits, byteLength):
+    """
+        Convert bits represetned using bianry string to normal string
+        Args:
+            bits: binary string
+            byteLength: length of expected output string
+    """
     s = ''
     charList = []
 
@@ -34,25 +38,32 @@ def bits2Str(bits, byteLength):
 
     return s
 
-# TODO: modify header strucutre for the line of headerlength, ack, syn, rst, fin to meet length requirement
-# Serialize normalized packet in fict from to a string
 def normalPacketSerialize(packet):
+    """
+        Serialize packet with all the fields filled (normalized packet)
+    """
     packetString = ""
-    packetString += bits2Str(str(bin(packet["sourcePort"]))[2:],2)
-    packetString += bits2Str(str(bin(packet["destPort"]))[2:],2)
-    packetString += bits2Str(str(bin(packet["seqNum"]))[2:],4)
-    packetString += bits2Str(str(bin(packet["ackNum"]))[2:],4)
-    packetString += bits2Str(str(bin(packet["headerLen"]))[2:] + str(bin(packet["ack"]))[2:] + str(bin(packet["rst"]))[2:] + str(bin(packet["syn"]))[2:] + str(bin(packet["fin"]))[2:], 1)
-    packetString += bits2Str(str(bin(packet["recvWindowSize"]))[2:], 3)
-    packetString += bits2Str(str(bin(packet["checksum"]))[2:],4)
-    if packet["headerLen"] != 0:
-        packetString += bits2Str(str(bin(packet["option"]))[2:], packet["headerLen"] * 4)
+    packetString += bits2Str(str(bin(int(packet["sourcePort"])))[2:],2)
+    packetString += bits2Str(str(bin(int(packet["destPort"])))[2:],2)
+    packetString += bits2Str(str(bin(int(packet["seqNum"])))[2:],4)
+    packetString += bits2Str(str(bin(int(packet["ackNum"])))[2:],4)
+    packetString += bits2Str(str(bin(int(packet["headerLen"])))[2:] + str(bin(int(packet["ack"])))[2:] + str(bin(int(packet["rst"])))[2:] + str(bin(int(packet["syn"])))[2:] + str(bin(int(packet["fin"])))[2:], 1)
+    packetString += bits2Str(str(bin(int(packet["recvWindowSize"])))[2:], 3)
+    packetString += bits2Str(str(bin(int(packet["checksum"])))[2:],4)
+    if int(packet["headerLen"]) != 0:
+        packetString += bits2Str(str(bin(int(packet["option"])))[2:], int(packet["headerLen"]) * 4)
     if "data" in packet:
-        packetString += packet["data"]    
+        packetString += packet["data"]
     return packetString
 
-# Serialize packet in dict form to a string
 def packetSerialize(packet):
+    """
+        Serialize any packet, it will first do the normalization then serialization
+        Args:
+            packet: packet in the form of dictionary
+        Returns:
+            packetString: packet in the from of binary string
+    """
     packetString = ""
     try:
         # normalize header with empty fields
@@ -62,11 +73,17 @@ def packetSerialize(packet):
         packetString = normalPacketSerialize(packet)
     except:
         print "Unexpected error in crp packet serialization"
-        raise 
+        raise
     return packetString
 
-# Deserialize a packet in string form into dict
 def packetDeserialize(packetString):
+    """
+        Deserialize any packet string
+        Args:
+            packetString: packet in the form of string
+        Returns:
+            packet: packet in the from of dictionary
+    """
     packet = {}
     try:
         packetBitString = str2Bits(packetString)
@@ -91,9 +108,14 @@ def packetDeserialize(packetString):
         raise
     return packet
 
-# Fletcher checksum
-# More details can be found on http://www.drdobbs.com/database/fletchers-checksum/184408761
 def fletcherCheckSum(packetString, k):
+    """
+        Fletcher CheckSum
+        More details can be found on http://www.drdobbs.com/database/fletchers-checksum/184408761
+        Args:
+            packetString: packet in the form of binary string
+            k: flecher constant
+    """
     if k not in FLETCHER_CONFIG:
         raise ValueError("Valid choices of k should be 16, 32 and 64")
     sum1 = 0
@@ -106,29 +128,29 @@ def fletcherCheckSum(packetString, k):
     sum2 = bin(sum2<<8)[2:]
     zero16 = "0000000000000000"
     return sum2+sum1+zero16
-#self defined queue class
+
 class Queue():
+    """
+        Self defined queue class
+    """
     def __init__(self):
         self.list = []
 
     def push(self,item):
-        "Enqueue the 'item' into the queue"
+        # Enqueue the 'item' into the queue
         self.list.insert(0,item)
 
     def pop(self):
-        """
-          Dequeue the earliest enqueued item still in the queue. This
-          operation removes the item from the queue.
-        """
+        # Dequeue the earliest enqueued item still in the queue. This operation removes the item from the queue.
         return self.list.pop()
-    
+
     def push_front(self,item):
         self.list.append(item)
-        
+
     def isEmpty(self):
-        "Returns true if the queue is empty"
+        # Returns true if the queue is empty"
         return len(self.list) == 0
-    
+
     def insert_inorder(self,item):
         self.list.append(item)
         self.list.sort(key=lambda x: x['seqNum']) 
@@ -138,8 +160,3 @@ class Queue():
 
     def length(self):
         return len(self.list)
-    
-        
-if __name__ == "__main__":
-    
-    
